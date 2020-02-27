455ef0b01d8478d3e3d3a9bcf72fdc7f82b09ef3
dernathul <lautaroparracab@gmail.com>
2020-02-27 16:59:09 +0100

    not wuite done with connecting front and back end

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from './components/InputFields';
+import LoginForm from "./components/LoginForm";
+import { authenticate } from './modules/auth';
 
 class App extends Component {
   state = {
     distance: "",
     gender: "female",
-    age: ""
+    age: "",
+    renderLoginForm: false,
+    autenticated: false,
+    message: ""
   };
 
   onChangeHandler = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  onLogin = async e => {
+    e.preventDefault();
+    const response = await Authenticate(
+      e.target.email.value
+    );
+    if (response.autenticated) {
+      this.setState({ autenticated: true });
+    } else {
+      this.setState({ message: response.message, renderLoginForm: false });
+    }
+  };
+
   render() {
+    const { renderLogin, autenticated, message } = this.state;
+    let renderLogin;
+    switch(true) {
+      case renderLoginForm && !autenticated:
+        renderLogin = <LoginForm submitFormHandler={this.onLogin} />
+        break;
+        case !renderLoginForm && !autenticated:
+          renderLogin = (
+          <>
+        <button
+          id="login"
+          onClick={() => this.setState({ renderLoginForm: true })}
+      >
+        Login
+      </button>
+          <p>{message}</p>
+          </>
+    );
+    break;
+    case authenticated:
+      renderLogin = (
+        <p>Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
+      );
+      break;
+    }
+
     return (
-      <>
+      <>      
         <InputFields onChangeHandler={this.onChangeHandler} />
+        {renderLogin}
         <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
@@ -24,6 +68,7 @@ class App extends Component {
         />  
       </>
     );
+    
   }
 }
 
